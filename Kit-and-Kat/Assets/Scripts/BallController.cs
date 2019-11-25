d44d6f29ad4a34130772e0a95940d6e098c805a2
ido.lazer <ido.lazer@mail.huji.ac.il>
2019-11-25 23:29:25 +0200

    trying to upload changes

---
+++ Kit-and-Kat/Assets/Scripts/BallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] float mKickForce = 10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * mKickForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Kit-and-Kat/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float mPlayerSpeed;
+    public Rigidbody2D mPlayer;
+    public Rigidbody2D ball;
+    public KeyCode mRightKey;
+    public KeyCode mLeftKey;
+
+    private KeyCode mThrowKey = KeyCode.Space;
+    public static bool IsHoldingBall = false;
+
+//    public static int mScore; 
+//    public static int mLife;
+//    public static Vector3 defaultPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(mRightKey))
+        {
+            mPlayer.AddForce(Vector2.right * Time.deltaTime * mPlayerSpeed);
+            
+        }
+        
+        else if (Input.GetKey(mLeftKey))
+        {
+            mPlayer.AddForce(Vector2.left * Time.deltaTime * mPlayerSpeed);
+        }
+
+        if (Input.GetKey(mThrowKey))
+        {
+            if (IsHoldingBall)
+            {
+                IsHoldingBall = false;
+                // throw the ball
+            }
+        }
+    }
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {    
+        //Check if the trigger is a ball trigger or cat  
+        //Update the game controller that you have the ball?
+
+        IsHoldingBall = true;
+        Debug.Log("Got the ball!");
+    }
+}
